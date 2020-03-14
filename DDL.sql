dcc23c3b3a0ac363c872ae78a9438a8283126cc5
yooo <brayanguitar000@gmail.com>
2020-03-13 21:37:57 -0500

    DDL corregido

--- DDL.sql
+++ DDL.sql
@@ -21,17 +21,17 @@ insert into empleado(id, nombre, apellido, cargo, salario, ciudad) values (2, 'J
 
 // registros en Remota
 
-insert into empleado(id, nombre, apellido, cargo, salario, ciudad) values (1, 'Maria', 'Castillo', 'Vendedor', 1000000, 'Medellin');
-insert into empleado(id, nombre, apellido, cargo, salario, ciudad) values (1, 'Juan', 'Garcia', 'Gerente', 2000000, 'Medellin');
+insert into empleado(id, nombre, apellido, cargo, salario, ciudad) values (3, 'Maria', 'Castillo', 'Vendedor', 1000000, 'Medellin');
+insert into empleado(id, nombre, apellido, cargo, salario, ciudad) values (4, 'Juan', 'Garcia', 'Gerente', 2000000, 'Medellin');
 
 
 create extension dblink;
 
 //Fragmentacion
 
-select empleado_R.*
-from dblink('dbname=bdremota port=5432 host=192.168.0.17 user postgres select * from empleado')
+
+select empleado_R.* 
+from dblink('dbname=bdlocal port=5432 host=10.28.1.65 password=123456 user = postgres','select * from empleado')
 as empleado_R(id integer, nombre varchar(50), apellido varchar(50), cargo varchar(50), salario real, ciudad varchar(50))
-UNION
-select * from empleado
+UNION select * from empleado;
 order by ciudad;
