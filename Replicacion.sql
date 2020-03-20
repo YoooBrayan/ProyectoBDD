1646770a0f44bb3badb09650aad1fe0b222f1c48
yooo <brayanguitar000@gmail.com>
2020-03-20 11:18:28 -0500

    Replicacion

--- Replicacion.sql
+++ Replicacion.sql
@@ -1,3 +1,11 @@
+/*Pasos:
+1. Modificar postresql.conf  en Maestro y esclavo --> wal-level replica --> reiniciar servicios
+2. añadir la ip a ph_hba
+3. Crear rol replica con permisos de replication
+4. otorgarle permisos al rol replica sobre la tabla a la que se le va hacer replicada
+*/
+
+
 /* Creae Rol */
 
 create role replica REPLICATION LOGIN PASSWORD '123456';
@@ -7,8 +15,16 @@ GRANT all on autor to replica;
 /* Publicaciones Maestro */
 
 /* Publicacion de autor */
-CREATE PUBLICATION public_autor FOR TABLE autor
+CREATE PUBLICATION public_autor FOR TABLE autor;
 
+
+/* Publicacion de libro */
+GRANT all on libro to replica; --> esto se hace con el usuario postgres
+CREATE PUBLICATION public_libro FOR TABLE libro
+
+
+/* Publicacion de editorial */
+CREATE PUBLICATION public_editorial FOR TABLE editorial
 
 /* Publicaciones Esclavo */
 
@@ -25,8 +41,12 @@ CREATE PUBLICATION public_autor FOR TABLE autor
 
 /* Subscripcion de autor */
 create subscription sub_autor
-connection 'host=192.168.0.15 port=5433 user=replica dbnama=libreria password=123456'
+connection 'host=127.0.0.1 port=5432 user=replica dbname=libreria password=123456'
 publication public_autor;
 
+/* Subscripcion de editorial */
+create subscription sub_libro
+connection 'host=127.0.0.1 port=5432 user=replica dbname=libreria password=123456'
+publication public_libro;
 
 /* Subscripciones Maestro */
